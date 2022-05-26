3dfe48978e4104f0de14fcdfa3a43da189be610e
jason <55410882+jason-vn@users.noreply.github.com>
2022-05-25 22:18:50 -0500

    Find triangle's area given base and height

---
+++ TriangleArea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int h = sc.nextInt();
+
+        System.out.println(0.5 * (b * h));
+    }
+}
