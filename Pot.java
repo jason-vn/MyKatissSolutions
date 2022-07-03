35ce986ae5970aea4032ee0b85032465679614d9
jason <55410882+jason-vn@users.noreply.github.com>
2022-07-03 13:13:24 -0500

    Update Pot.java

--- Pot.java
+++ Pot.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
 
 public class Pot {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int addends = sc.nextInt();
-        String[] strArr = null;
-        int base = 0;
-        int power = 0;
-        int total = 0;
-        int count = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int addends = sc.nextInt();
+		String[] strArr = null;
+		int base = 0;
+		int power = 0;
+		int total = 0;
+		int count = 0;
 
-        while (count != addends) {
-            // Separate number into array of digits
-            String s = sc.next();
-            strArr = s.split("");
-            // New array with length of # of digits
-            int[] arr = new int[strArr.length];
-            // Load string array of digits into int array
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = Integer.parseInt(strArr[i]);
-            }
+		while (count != addends) {
+			// Separate number into array of digits
+			String s = sc.next();
+			strArr = s.split("");
+			// New array with length of # of digits
+			int[] arr = new int[strArr.length];
+			// Load string array of digits into int array
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = Integer.parseInt(strArr[i]);
+			}
 
-            // Exclude last character of input to get base number
-            String baseStr = s.substring(0, s.length() - 1);
-            base = Integer.parseInt(baseStr);
+			// Exclude last character of input to get base number
+			String baseStr = s.substring(0, s.length() - 1);
+			base = Integer.parseInt(baseStr);
 
-            // Last num of array is power raised to
-            power = arr[arr.length - 1];
+			// Last num of array is power raised to
+			power = arr[arr.length - 1];
 
-            // Append base^power to running total
-            int output = (int) Math.pow(base, power);
-            total += output;
-            
-            // Increment counter for tracking input lines
-            count++;
-        }
+			// Append base^power to running total
+			int output = (int) Math.pow(base, power);
+			total += output;
 
-        System.out.println(total);
+			// Increment counter for tracking input lines to break out of loop
+			count++;
+		}
 
-    }
+		System.out.println(total);
+		sc.close();
+	}
 }
